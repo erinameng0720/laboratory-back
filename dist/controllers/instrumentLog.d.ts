c3f012c944ab65bbe1f60a42b8efc9ffb72ae84d
erinameng0720 <erinameng0720@gmail.com>
2026-08-30 00:27:17 +0800

    commit

--- dist/controllers/instrumentLog.d.ts
+++
@@ -1,6 +0,0 @@
-import type { Request, Response } from 'express';
-/** GET /instrument-log */
-export declare const getAll: (_req: Request, res: Response) => Promise<void>;
-/** GET /instrument-log/:id */
-export declare const getId: (req: Request, res: Response) => Promise<void>;
-//# sourceMappingURL=instrumentLog.d.ts.map
